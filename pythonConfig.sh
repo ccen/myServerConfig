e5aa59521bb6294692b48598669fb8b6676cc067
ccen <mail2cchen@gmail.com>
2017-02-26 05:00:13 +0800

    add python install configuration

---
+++ pythonConfig.sh
@@ -0,0 +1,6 @@
+sudo apt-get install python2.7 python2.7-dev
+sudo apt-get install build-essential libssl-dev libevent-dev libjpeg-dev libxml2-dev libxslt-dev
+sudo apt-get install  python-pip
+sudo pip install --upgrade pip
+sudo pip install virtualenv
+
