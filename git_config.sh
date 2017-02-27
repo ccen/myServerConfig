d606e3bbd8606b392bd92a45663a6001e211b66e
ccen <mail2cchen@gmail.com>
2017-02-28 01:45:09 +0800

    add git configuration

---
+++ git_config.sh
@@ -0,0 +1,15 @@
+
+# git configuration
+# alias
+git config --global alias.br branch
+git config --global alias.ci commit
+git config --global alias.co checkout
+git config --global alias.st status
+git config --global alias.lg "log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen (%cr) %C(bold blue)<%an>%Creset' --abbrev-commit --"
+
+# user info
+git config --global user.name ccen
+git config --global user.email mail2cchen@gmail.com
+
+# color
+git config --global color.ui true
