b473a73b521277562450d34df31f755f6e506193
ccen <mail2cchen@gmail.com>
2017-09-05 04:43:49 +0800

    amend file path for bitcoind script

--- bitcoind_depen.sh
+++ bitcoind_depen.sh
@@ -1,24 +1,28 @@
 
 # install bitcoind dependency
 
+sudo apt-get install build-essential
+sudo apt-get install libgtk2.0-dev
+sudo apt-get install libssl-dev
+
 if [ ! -d ~/util ]; then
   mkdir ~/util
 fi
 
 current_path=`pwd`
 cd ~/util
-wget http://miniupnp.free.fr/files/download.php?file=miniupnpc-1.5.20110215.tar.gz -O miniupnpc-1.5.20110215.tar.gz
+wget http://miniupnp.free.fr/files/download.php?file=miniupnpc-1.5.tar.gz -O miniupnpc-1.5.tar.gz
 wget http://download.oracle.com/berkeley-db/db-4.7.25.NC.tar.gz
 wget http://sourceforge.net/projects/boost/files/boost/1.57.0/boost_1_57_0.tar.gz
 
-tar -xzvf miniupnpc-1.5.20110215.tar.gz
-cd miniupnpc-1.5.20110215
+tar -xzvf miniupnpc-1.5.tar.gz
+cd miniupnpc-1.5
 make
 sudo make install
 
 cd ~/util
 tar -xzvf db-4.7.25.NC.tar.gz
-cd db-4.7.25.NC.tar.gz/build_unix
+cd db-4.7.25.NC/build_unix
 ../dist/configure --enable-cxx
 make
 
