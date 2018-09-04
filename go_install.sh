a6ed4c0dd24f65d203cfcbb1e505487ec836c8e9
Ubuntu <ccen@ccen-ethPrePlay.d22ddpel134ezis4yfawpyty2f.cx.internal.cloudapp.net>
2018-09-04 10:31:15 +0000

    update golang version

--- go_install.sh
+++ go_install.sh
@@ -1,12 +1,14 @@
 
-#install golang1.8
+#install golang1.$version
+version=11
+
 if [ ! -d /usr/local/go ]; then
     current_path=`pwd`
     cd ~
-    if [ ! -f go1.8.linux-amd64.tar.gz ]; then
-        wget https://storage.googleapis.com/golang/go1.8.linux-amd64.tar.gz
+    if [ ! -f go1.$version.linux-amd64.tar.gz ]; then
+        wget https://storage.googleapis.com/golang/go1.$version.linux-amd64.tar.gz
     fi
-    sudo tar -C /usr/local -xzf go1.8.linux-amd64.tar.gz
+    sudo tar -C /usr/local -xzf go1.$version.linux-amd64.tar.gz
 
     if [ ! -d ~/go ]; then
         mkdir go
@@ -18,7 +20,7 @@ if [ ! -d /usr/local/go ]; then
     echo -e "export PATH=\$PATH:/usr/local/go/bin:\$GOPATH/bin" >> .profile
     
     source .profile
-    rm go1.8.linux-amd64.tar.gz
+    rm go1.$version.linux-amd64.tar.gz
     cd ${current_path}
 fi
 
